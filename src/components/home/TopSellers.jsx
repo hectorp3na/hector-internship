2b47cdaae22549fe6839235eb9b416c95de809e7
Hector <hectorpena6590@gmail.com>
2025-06-10 13:43:43 -0700

    top sellers-authorId

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+const [topSellers, setTopSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getTopSellers = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+        );
+        setTopSellers(response.data);
+      } catch (error) {
+        console.error("Failed to load collection:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getTopSellers();
+  }, []);
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +35,21 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {topSellers.map((seller, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    <Link to={`/author/${seller.authorId}`}>
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={seller.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">{seller.authorName}</Link>
+                    <span>{seller.price} ETH</span>
                   </div>
                 </li>
               ))}
