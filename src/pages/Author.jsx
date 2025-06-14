62cec7e6b96415516155fb72f216e623c796ce6d
Hector <hectorpena6590@gmail.com>
2025-06-14 01:04:26 -0700

    author page setup

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,31 +1,41 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useParams, Link } from "react-router-dom";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+
+const skeletonStyle = {
+  background: "linear-gradient(90deg, #f0f0f0 25%, #e0e0e0 50%, #f0f0f0 75%)",
+  backgroundSize: "200% 100%",
+  animation: "skeleton-loading 1.2s infinite linear",
+};
 
 const Author = () => {
-    const [author, setAuthor] = useState({});
-      const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const [author, setAuthor] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [followed, setFollowed] = useState(false)
 
-      useEffect(() => {
-        const getAuthor = async () => {
-          try {
-            const response = await axios.get(
-              "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012"
-            );
-            setAuthor(response.data);
-          } catch (error) {
-            console.error("Failed to load collection:", error);
-          } finally {
-            setLoading(false);
-          }
-        };
-    
-        getAuthor();
-      }, []);
-    
+  useEffect(() => {
+    const getAuthor = async () => {
+      try {
+        const response = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`
+        );
+        setAuthor(response.data);
+      } catch (error) {
+        console.error("Failed to load collection:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getAuthor();
+  }, [id]);
+
+  const handleFollowClick = () => {
+    setFollowed(!followed);
+  };
 
 
   return (
@@ -45,44 +55,127 @@ const Author = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={author.authorImage} alt="" />
-                      <i className="fa fa-check"></i>
+                {loading ? (
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div
+                        style={{
+                          ...skeletonStyle,
+                          width: 150,
+                          height: 150,
+                          borderRadius: "50%",
+                          marginBottom: 20,
+                        }}
+                      ></div>
                       <div className="profile_name">
-                        <h4>
-                         {author.authorName}
-                          <span className="profile_username">@{author.tag}</span>
-                          <span id="wallet" className="profile_wallet">
-                           {author.walletAddress}
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                        <div
+                          style={{
+                            ...skeletonStyle,
+                            width: 200,
+                            height: 24,
+                            marginBottom: 10,
+                            borderRadius: 4,
+                          }}
+                        ></div>
+                        <div
+                          style={{
+                            ...skeletonStyle,
+                            width: 150,
+                            height: 16,
+                            marginBottom: 10,
+                            borderRadius: 4,
+                          }}
+                        ></div>
+                        <div
+                          style={{
+                            ...skeletonStyle,
+                            width: 250,
+                            height: 16,
+                            borderRadius: 4,
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div
+                          style={{
+                            ...skeletonStyle,
+                            width: 100,
+                            height: 20,
+                            marginBottom: 10,
+                            borderRadius: 4,
+                          }}
+                        ></div>
+                        <div
+                          style={{
+                            ...skeletonStyle,
+                            width: 80,
+                            height: 30,
+                            borderRadius: 8,
+                          }}
+                        ></div>
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
+                ) : (
+                  <div className="d_profile de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">{author.followers} followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      <div className="profile_avatar">
+                        <img src={author.authorImage} alt="" />
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            {author.authorName}
+                            <span className="profile_username">
+                              @{author.tag}
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              {author.address}
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          {author.followers + (followed ? 1 : 0)} followers
+                        </div>
+                        <button onClick={handleFollowClick} className="btn-main">
+                        {followed ? "Unfollow" : "Follow"}
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems authorId={id} />
                 </div>
               </div>
             </div>
           </div>
         </section>
+        
+        <style>
+          {`
+          @keyframes skeleton-loading {
+            0% {
+              background-position: -200% 0;
+            }
+            100% {
+              background-position: 200% 0;
+            }
+          }
+        `}
+        </style>
       </div>
     </div>
   );
