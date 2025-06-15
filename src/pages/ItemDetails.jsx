6b8a299406449bd2b3246d58492e4731007435b3
Hector <hectorpena6590@gmail.com>
2025-06-15 01:04:39 -0700

    itemDetails dynamic

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,14 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 import AuthorImage from "../images/author_thumbnail.jpg";
 import nftImage from "../images/nftImage.jpg";
 
 const ItemDetails = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  const { nftId } = useParams();
+  const [itemDetails, setItemDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const getItemDetails = async () => {
+      try {
+        const response = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
+        );
+        setItemDetails(response.data);
+      } catch (error) {
+        console.error("Failed to load item:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getItemDetails();
+    window.scrollTo(0, 0);
+  }, [nftId]);
+
+  if (loading) {
+    return (
+      <div className="container text-center" style={{ padding: "100px 0" }}>
+        <h4>Loading item details...</h4>
+      </div>
+    );
+  }
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -18,42 +44,46 @@ const ItemDetails = () => {
             <div className="row">
               <div className="col-md-6 text-center">
                 <img
-                  src={nftImage}
+                  src={itemDetails.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
                   alt=""
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>
+                    {itemDetails.title} #{itemDetails.tag}
+                  </h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
                       <i className="fa fa-eye"></i>
-                      100
+                      {itemDetails.views}
                     </div>
                     <div className="item_info_like">
                       <i className="fa fa-heart"></i>
-                      74
+                      {itemDetails.likes}
                     </div>
                   </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
+                  <p>{itemDetails.description}</p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${itemDetails.ownerId}`}>
+                            <img
+                              className="lazy"
+                              src={itemDetails.ownerImage}
+                              alt=""
+                            />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${itemDetails.ownerId}`}>
+                            {itemDetails.ownerName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -64,13 +94,19 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${itemDetails.creatorId}`}>
+                            <img
+                              className="lazy"
+                              src={itemDetails.creatorImage}
+                              alt=""
+                            />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${itemDetails.creatorId}`}>
+                            {itemDetails.creatorName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -78,7 +114,7 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>{itemDetails.price}</span>
                     </div>
                   </div>
                 </div>
